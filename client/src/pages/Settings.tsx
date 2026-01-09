d9c857498d1529e30a8b37e9cc635db1870ea382
niekelodeon <niekamperse2@proton.me>
2026-01-09 17:30:07 +0100

    Settings.tsx layout updated

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -4,8 +4,8 @@ import RemoteStructure from '../components/RemoteStructure'
 export default function Settings() {
     return (
         <div id="container-page" className="max-h-screen overflow-hidden">
-            <div id="container-layout" className="h-full">
-                <div className="container-connection">
+            <div id="container-layout" className="flex flex-row justify-center gap-[25%] pt-20 pr-[7%] pb-10 pl-[10%]">
+                <div id="container-Connection">
                     <Connection />
                 </div>
 
